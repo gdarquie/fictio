6bceeb4fed9a42b4f6917c46270a2c5bd14efff0
gdarquie <gaetan.darquie@gmail.com>
2020-03-03 20:31:44 +0100

    FI-120: add MUI implementation of Material Design

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Layout from '../components/Layout';
 import Head from "next/head";
 import Link from 'next/link';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 const Index = (props) => {
     const fictionUuid = '1b7df281-ae2a-40bf-ad6a-ac60409a9ce6';
@@ -10,12 +13,21 @@ const Index = (props) => {
             <Head>
                 <title>Ficti : create your fiction</title>
                 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
             </Head>
     
             <Layout>
                 <Link href="/fictions/1b7df281-ae2a-40bf-ad6a-ac60409a9ce6">
                     <a>My Fiction</a>
                 </Link>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+                <Card>
+                    <CardContent>
+                        hello
+                    </CardContent>
+                </Card>
             </Layout>
         </div>
     );
